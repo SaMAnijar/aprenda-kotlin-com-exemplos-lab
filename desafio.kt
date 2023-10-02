c223fed2cce09b9a17e1c845298cc27f1d20b828
Samuel B Anijar <128177565+SaMAnijar@users.noreply.github.com>
2023-10-02 16:41:00 -0300

    Minhas alterações

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,49 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome")
+    }
+
+    fun listarConteudos() {
+        println("Conteúdos da formação $nome:")
+        for (conteudo in conteudos) {
+            println("- ${conteudo.nome} (${conteudo.duracao} minutos)")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criar alguns conteúdos educacionais
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 120)
+    val conteudo2 = ConteudoEducacional("Desenvolvilmento Mobile com Kotlin", 90)
+
+    // Criar uma formação
+    val formacao = Formacao("Desenvolvedor Kotlin", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2))
+
+    // Criar alguns usuários
+    val usuario1 = Usuario("Samuel")
+    val usuario2 = Usuario("Ana")
+
+    // Matricular os usuários na formação
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    // Verificar os inscritos na formação
+    println("Inscritos na formação ${formacao.nome}:")
+    for (inscrito in formacao.inscritos) {
+        println("- ${inscrito.nome}")
+    }
+
+    // Listar os conteúdos da formação
+    formacao.listarConteudos()
 }
